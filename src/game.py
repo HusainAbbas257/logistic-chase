d3e4d6bb4bad5c6019664965ad4abfe38e0635db
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2026-04-24 18:05:42 +0530

    added a basic black screen for later

---
+++ src/game.py
@@ -0,0 +1,30 @@
+import math
+import time
+
+import pygame
+import random
+pygame.init()
+screen=pygame.display.set_mode((1200,720),)
+font=pygame.font.SysFont('arial',32)
+width,height=screen.get_width(),screen.get_height()
+clock=pygame.time.Clock()
+
+def mainloop():
+    running=True
+    while running:
+        screen.fill((0,0,0))
+        # events 
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+
+        # drawing
+        screen.blit(font.render(f'fps:{int(clock.get_fps())}',True,(100,100,100)),(10,10))
+            
+        pygame.display.flip()
+        clock.tick(60)
+        
+
+
+if __name__=='__main__':
+    mainloop()
