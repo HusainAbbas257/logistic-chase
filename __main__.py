fc604f66c8c3eea0f77c3ece8825f43a5a31833a
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2026-04-25 14:14:29 +0530

    forgot to commit this :>

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import src.game as game
 import pandas as pd
 
 
-df = pd.DataFrame(game.mainloop(input('type of opponent->')), columns=["ai x", "ai y", "ai vx",'ai vy',"human x", "human y", "human vx",'human vy','w','a','s','d','up','left','down','right'])
+df = pd.DataFrame(game.mainloop(input('type of opponent->')), columns=["ai x", "ai y", "ai vx",'ai vy',"human x", "human y", "human vx",'human vy','up','left','down','right'])
 
 df.drop_duplicates(inplace=True)
 print('data retrieved->',df.shape)
